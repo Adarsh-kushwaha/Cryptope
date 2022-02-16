85827aa96a822b50315080e687346fe1c7d0f21a
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-02-16 17:27:09 +0530

    transaction part completed

--- web3/src/components/Welcome.jsx
+++ web3/src/components/Welcome.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import { TransactionContext } from '../context/TransactionContext';
 
 const Input = ({ placeholder, type, name, value, handleChange }) => {
     return <input type={type} value={value} placeholder={placeholder} step="0.001" onChange={(e) => handleChange(e, name)} className="py-3 px-4 mx-2 w-full bg-gray-200 outline-none text-gray-800 font-semibold rounded-sm" />
@@ -8,37 +9,42 @@ const Welcome = () => {
 
     const [isLoading, setIsLoading] = useState(false);
 
-    const walletHandler = () => {
+    const { connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext)
 
-    }
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
+        e.preventDefault();
 
-    const handleSubmit =()=>{
-
+        if (addressTo || amount || keyword || message) {
+            return sendTransaction()
+        }
     }
 
     return (
         <div className='mt-8'>
-            <button
+            {!currentAccount && <button
                 type="button"
                 className='w-full bg-green-500 text-gray-100 py-4 px-6 text-xl rounded-md cursor-pointer hover:bg-green-700 text-center rounded-md'
-                onClick={walletHandler}
+                onClick={connectWallet}
             >
                 Connect To Wallet
-            </button>
+            </button>}
+            {currentAccount && <p className='text-center text-green-600 my-6 text-2xl font-bold'> ✅ You are connected to your wallet</p>}
+
             <div className='w-full p-4 mt-8 grid grid-rows grid-cols-1 gap-4 '>
                 <h2 className='text-xl font-bold'>Transaction Form</h2>
                 <form className='grid grid-rows grid-cols-1 gap-4'>
-                    <Input placeholder="Address To" type="text" handleChange={() => { }} name="Address To" />
-                    <Input placeholder="Ether (Amt)" type="number" handleChange={() => { }} name="amount" />
-                    <Input placeholder="Keyword (gif)" type="text" handleChange={() => { }} name="keyword" />
-                    <Input placeholder="Message" type="text" handleChange={() => { }} name="message" />
-                
+                    <Input placeholder="Address To" type="text" handleChange={handleChange} name="addressTo" />
+                    <Input placeholder="Ether (Amt)" type="number" handleChange={handleChange} name="amount" />
+                    <Input placeholder="Keyword (gif)" type="text" handleChange={handleChange} name="keyword" />
+                    <Input placeholder="Message" type="text" handleChange={handleChange} name="message" />
+
                     {isLoading === false ? <button
                         type="button"
                         className='w-full bg-blue-400 text-gray-100 py-2 px-4 text-xl rounded-md cursor-pointer hover:bg-blue-500 text-center rounded-md mx-2'
                         onClick={handleSubmit}
                     >Send Now</button> : <p>Loading..</p>}
-                    
+
                 </form>
             </div>
         </div>
---
+++ web3/src/context/TransactionContext.jsx
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { contractAddress, contractABI } from "../utils/Contant";
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+}
+
+export const TransactionProvider = ({ children }) => {
+
+    const [currentAccount, setCurrentAccount] = useState("");
+    const [formData, setFormData] = useState({ addressTo: "", amount: "", keyword: "", message: "" })
+    const [isLoading, setIsLoading] = useState(false);
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"))
+
+    const handleChange = (e, name) => {
+        setFormData((prevState) => ({ ...prevState, [name]: e.target.value }))
+    }
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            if (!ethereum) {
+                return alert("Please Install Metamask");
+            }
+
+            const accounts = await ethereum.request({ method: "eth_accounts" });
+
+            console.log(accounts);
+
+            if (accounts.length) {
+                setCurrentAccount(accounts[0]);
+            } else {
+                console.log("No Account Found")
+            }
+        } catch (error) {
+            console.log(error);
+            throw new error("No ethereum object found!")
+        }
+
+
+    }
+
+    const connectWallet = async () => {
+        try {
+            if (!ethereum) {
+                return alert("Please Install Metamask");
+            }
+
+            const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+            setCurrentAccount(accounts);
+        } catch (error) {
+            console.log(error);
+            throw new error("No ethereum object found!")
+        }
+
+    }
+
+
+    const sendTransaction = async () => {
+        try {
+            if (!ethereum) {
+                return alert("Please Install Metamask");
+            }
+
+            const { addressTo, amount, keyword, message } = formData;
+            const transactionContract = getEthereumContract();
+            const parsedAmount = ethers.utils.parseEther(amount);
+            await ethereum.request({
+                method: "eth_sendTransaction",
+                params: [{
+                    from: currentAccount,
+                    to: addressTo,
+                    gas: "0x5208",
+                    value: parsedAmount._hex,
+                }]
+            })
+
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, keyword, message);
+            setIsLoading(true);
+            console.log(`loading... ${transactionHash.hash}`);
+            await transactionHash.wait();
+            setIsLoading(false);
+            console.log(`success ${transactionHash.hash}`);
+
+            const transactionCount = await transactionContract.getTransactionCount();
+            setTransactionCount(transactionCount.toNumber());
+
+
+
+        } catch (error) {
+
+        }
+    }
+
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, [])
+
+
+    return (
+        <TransactionContext.Provider value={{
+            connectWallet: connectWallet,
+            currentAccount: currentAccount,
+            formData: formData,
+            sendTransaction: sendTransaction,
+            handleChange: handleChange
+        }}>
+            {children}
+        </TransactionContext.Provider>
+    )
+}
