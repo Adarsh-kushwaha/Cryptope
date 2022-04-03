1fb7b5dfb7f3eca22a5dab0a8113014109dd0bd8
Adarsh Kushwaha <57234525+Adarsh-kushwaha@users.noreply.github.com>
2022-04-03 23:22:28 +0530

    Update Navbar.jsx

--- web3/src/components/Navbar.jsx
+++ web3/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
     <div className='flex flex-row justify-between mt-4'>
-        <div className='text-2xl font-bold'>Web 3.0</div>
+        <div className='text-2xl font-bold'>Crypto Pe</div>
         <div className='bg-blue-500 text-gray-100 py-2 px-6 text-xl rounded-md cursor-pointer'>Login</div>
     </div>
     </>
