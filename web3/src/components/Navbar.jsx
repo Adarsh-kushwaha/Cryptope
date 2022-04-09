f3b8c230ed5007ccdc8880fa05e69e24389f7050
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-04-09 11:08:39 +0530

    Configured for mobile responsiveness

--- web3/src/components/Navbar.jsx
+++ web3/src/components/Navbar.jsx
@@ -2,18 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <>
-<<<<<<< HEAD
+
     <div className='flex flex-row justify-between my-6 pb-5 border-b-2 border-gray-500'>
-        <div className='text-2xl font-bold'>Web 3.0</div>
-        <div className='bg-[#161B22] text-gray-100 py-2 px-6 text-xl rounded-md cursor-pointer'><a href="https://github.com/Adarsh-kushwaha/Web-3.0" target="_blank" rel='noreferrer'>GitHub</a></div>
-=======
-    <div className='flex flex-row justify-between mt-4'>
+      <div className='text-2xl font-bold'>Web 3.0</div>
+      <div className='bg-[#161B22] text-gray-100 py-2 px-6 text-xl rounded-md cursor-pointer'><a href="https://github.com/Adarsh-kushwaha/Web-3.0" target="_blank" rel='noreferrer'>GitHub</a></div>
+      <div className='flex flex-row justify-between mt-4'>
         <div className='text-2xl font-bold'>Crypto Pe</div>
         <div className='bg-blue-500 text-gray-100 py-2 px-6 text-xl rounded-md cursor-pointer'>Login</div>
->>>>>>> 774eaeddb1609e03db91524b8feca862697f72eb
+      </div>
     </div>
-    </>
+
   )
 }
 
